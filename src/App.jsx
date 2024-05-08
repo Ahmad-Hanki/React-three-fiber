4567a607337f293bfbebf12e72c449ccb5a31d57
ahmad <itxti909@gmail.com>
2024-05-08 18:55:31 +0300

    Geometrys

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,53 @@ import { useRef } from "react";
 //mesh renders any 3d objects
 //mesh needs geometry
 
+const Tours = ({ position, args, color }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <torusGeometry args={args} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+const ToursKNot = ({ position, args, color }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <torusKnotGeometry args={args} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const Sphere = ({ position, size, color }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={color} wireframe/>
+    </mesh>
+  );
+};
+
 const Cube = ({ position, size, color }) => {
   const ref = useRef();
 
@@ -12,7 +59,7 @@ const Cube = ({ position, size, color }) => {
     // delta is the deference time between last and current frame
     ref.current.rotation.x += delta;
     ref.current.rotation.y += delta * 2;
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) *2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime);
   });
 
   return (
@@ -30,14 +77,19 @@ function App() {
       <directionalLight position={[0, 0, 2]} intensity={0.5} />
 
       <ambientLight intensity={0.1} />
-      {/* <group position={[0,-1,0]}>
-        <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
-        <Cube position={[-1, 0, 0]} color={"pink"} size={[1, 1, 1]} />
-        <Cube position={[1, 2, 0]} color={"hotpink"} size={[1, 1, 1]} />
-        <Cube position={[-1, 2, 0]} color={"lightblue"} size={[1, 1, 1]} />
-      </group> */}
 
-      <Cube position={[0, 0, 0]} color={"lightblue"} size={[1, 1, 1]} />
+      <Cube position={[2, 0, 0]} color={"lightblue"} size={[1, 1, 1]} />
+      <Sphere position={[0, 0, 0]} color={"lightblue"} size={[1, 30, 30]} />
+      <Tours
+        position={[-2, 0, 0]}
+        color={"lightblue"}
+        args={[0.8, 0.1, 30, 30]}
+      />
+      <ToursKNot
+        position={[0, 2, 0]}
+        color={"lightblue"}
+        args={[0.5, 0.1, 1000, 50]}
+      />
     </Canvas>
   );
 }
